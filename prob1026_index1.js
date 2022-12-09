3ec93ba9eaf0a4646e3dffa0519631fde376ea81
John J Setlock II <johnsetlock@gmail.com>
2022-12-09 09:04:41 -0600

    progress 22-12-09

---
+++ prob1026_index1.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxAncestorDiff = function(root) {
+    let result = 0;
+     
+     const dfs = (node, currmax, currmin) => {
+         result = Math.max(result, Math.abs(currmax - currmin))
+         
+         if (node.left) {
+             const max = Math.max(currmax, node.left.val)
+             const min = Math.min(currmin, node.left.val)
+             dfs(node.left, max, min)
+         }
+         
+         if (node.right) {
+             const max = Math.max(currmax, node.right.val)
+             const min = Math.min(currmin, node.right.val)
+             dfs(node.right, max, min)
+         }
+     }
+     
+     dfs(root, root.val, root.val)
+     
+     return result
+ 
+ 
+ };
